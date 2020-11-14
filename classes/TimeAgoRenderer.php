05408cb80ad50fffda0dce68fb21b1b2034c505f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-11-14 16:55:59 +0100

    fix: hash amp script

--- classes/TimeAgoRenderer.php
+++ classes/TimeAgoRenderer.php
@@ -23,72 +23,92 @@ class TimeAgoRenderer {
   static function getEnqueuedScripts() {
     if(!self::$enqueueScript) return '';
     return '
-<script id="' . self::$scriptName . '" type="text/plain" target="amp-script">
-const content = document.querySelector("time");
-const timestamp = content.getAttribute("data-timestamp-s");
-updateTimeAgo();
-
-function updateTimeAgo() {
-  content.innerHTML="◉ " + getKatoFunkyTimeAgo(Number(timestamp));
-  setTimeout(() => updateTimeAgo(), 10 * 1000);
-}
-
-function getKatoFunkyTimeAgo(timestamp) {
-  const zdrowaskaS = 20; // 20 seconds
-  const dziesiatkaRozancaS = 5 * 60; // 5 minutes
-  const czescRozanzaS = 25 * 60; // 25 minutes
-  const hourS = 3600;
-  const dayS = 24 * 3600;
-  const weekS = 7 * 24 * 3600;
-  const miesiaceDopelniacz = ["stycznia", "lutego", "marca", "kwietnia", "maja", "czerwca", "lipca", "sierpnia", "września", "października", "listopada", "grudnia"];
-
-  const difference = (Date.now()/1000) - timestamp;
-
-  if (difference < 90) {
-    return "Właśnie teraz";
-  }
-  else if (difference < zdrowaskaS * 20) {
-    const zdrowaski = Math.floor(difference / zdrowaskaS);
-    return zdrowaski + " zdrowasiek temu";
-  }
-  else if (difference < czescRozanzaS) {
-    const no = Math.floor(difference / dziesiatkaRozancaS);
-    if (no == 1) return "Dziesiątkę różańca temu";
-    else if (no < 5) return no + " dziesiątki różańca temu";
-    else return no + " dziesiątek różańca temu";
-  }
-  else if (difference < 11 * czescRozanzaS) {
-    const no = Math.floor(difference / czescRozanzaS);
-    if (no == 1) return "Część różańca temu";
-    else return no + " części różańca temu";
-  }
-  else if (difference < dayS) {
-    const no = Math.floor(difference / hourS);
-    if (no < 2) return "Godzinę temu";
-    else if (no < 5) return no + " godziny temu";
-    else return no + " godzin temu";
-  }
-  else if (difference < weekS) {
-    const no = Math.floor(difference / dayS);
-    if (no < 2) return "Wczoraj";
-    else return no + " dni temu";
-  }
-  else if (difference < 4 * weekS) {
-    const no = Math.floor(difference / weekS);
-    if (no == 1) return "Tydzień temu";
-    else if (no < 5) return no + " tegodnie temu";
-    else return no + " tygodni temu";
-  }
-
-  const date = new Date(timestamp * 1000);
-  return date.getDate() + " " + miesiaceDopelniacz[date.getMonth()] + " " + date.getYear();
-}
-</script>
+<script id="' . self::$scriptName . '" type="text/plain" target="amp-script">' . self::getScriptContents() . '</script>
     ';
+  }
+
+  static function getScriptMeta() {
+    return '<meta name="amp-script-src" content="' . self::getScriptHash() . '" />';
+  }
+
+  static function getScriptHash() {
+    $contents = self::getScriptContents();
+    $hash = hash('sha384', $contents, true);
+    $out = base64_encode($hash);
+    $out = str_replace('=', '', $out);
+    $out = str_replace('+', '-', $out);
+    $out = str_replace('/', '_', $out);
+    return 'sha384-' . $out;
+  }
+
+  static function getScriptContents() {
+    return 'const content = document.querySelector("time");
+    const timestamp = content.getAttribute("data-timestamp-s");
+    updateTimeAgo();
+
+    function updateTimeAgo() {
+      content.innerHTML="◉ " + getKatoFunkyTimeAgo(Number(timestamp));
+      setTimeout(() => updateTimeAgo(), 10 * 1000);
+    }
+
+    function getKatoFunkyTimeAgo(timestamp) {
+      const zdrowaskaS = 20; // 20 seconds
+      const dziesiatkaRozancaS = 5 * 60; // 5 minutes
+      const czescRozanzaS = 25 * 60; // 25 minutes
+      const hourS = 3600;
+      const dayS = 24 * 3600;
+      const weekS = 7 * 24 * 3600;
+      const miesiaceDopelniacz = ["stycznia", "lutego", "marca", "kwietnia", "maja", "czerwca", "lipca", "sierpnia", "września", "października", "listopada", "grudnia"];
+
+      const difference = (Date.now()/1000) - timestamp;
+
+      if (difference < 90) {
+        return "Właśnie teraz";
+      }
+      else if (difference < zdrowaskaS * 20) {
+        const zdrowaski = Math.floor(difference / zdrowaskaS);
+        return zdrowaski + " zdrowasiek temu";
+      }
+      else if (difference < czescRozanzaS) {
+        const no = Math.floor(difference / dziesiatkaRozancaS);
+        if (no == 1) return "Dziesiątkę różańca temu";
+        else if (no < 5) return no + " dziesiątki różańca temu";
+        else return no + " dziesiątek różańca temu";
+      }
+      else if (difference < 11 * czescRozanzaS) {
+        const no = Math.floor(difference / czescRozanzaS);
+        if (no == 1) return "Część różańca temu";
+        else return no + " części różańca temu";
+      }
+      else if (difference < dayS) {
+        const no = Math.floor(difference / hourS);
+        if (no < 2) return "Godzinę temu";
+        else if (no < 5) return no + " godziny temu";
+        else return no + " godzin temu";
+      }
+      else if (difference < weekS) {
+        const no = Math.floor(difference / dayS);
+        if (no < 2) return "Wczoraj";
+        else return no + " dni temu";
+      }
+      else if (difference < 4 * weekS) {
+        const no = Math.floor(difference / weekS);
+        if (no == 1) return "Tydzień temu";
+        else if (no < 5) return no + " tegodnie temu";
+        else return no + " tygodni temu";
+      }
+
+      const date = new Date(timestamp * 1000);
+      return date.getDate() + " " + miesiaceDopelniacz[date.getMonth()] + " " + date.getYear();
+    }';
   }
 }
 
 function time_ago_footer_hook() {
   echo TimeAgoRenderer::getEnqueuedScripts();
+}
+
+function time_ago_head_hook() {
+  echo TimeAgoRenderer::getScriptMeta();
 }
 ?>
